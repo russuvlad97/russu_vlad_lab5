b151ff16439fd2656ee151c3d6c0d9ed653dba5a
russuvlad97 <russu.vlad97@gmail.com>
2021-11-05 17:40:34 +0200

    Add project files.

---
+++ russu_vlad_lab5/Models/ExpenseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace russu_vlad_lab5.Models
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
